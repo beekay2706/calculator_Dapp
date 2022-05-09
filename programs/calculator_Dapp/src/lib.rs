3cb46c75a0b46990eb8bad0fcbd179c306928c89
beekay2706 <barathas12@gmail.com>
2022-05-09 06:09:01 +0530

    fix errors

--- programs/calculator_Dapp/src/lib.rs
+++ programs/calculator_Dapp/src/lib.rs
@@ -1,28 +1,25 @@
 use anchor_lang::solana_program::entrypoint::ProgramResult;
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("8UsT1RSVr7mYLPfMQ6FB6y5PhtW9ugXXuK7p5KoqMW9N");
 
 #[program]
-pub mod calculator_dapp {
+pub mod mycalculatordapp {
     use super::*;
-    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult 
-    {
-      let calculator = ctx.accounts.calculator;
-      calculator.greeting = init_message;
-      Ok(())
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
+        Ok(())
     }
 }
- #[derive(Accounts)]
- pub struct Create<'info> {
-     #[account(init, payer = user, space = 264)]
-     pub calculator: Accounts<'info , Calculator>,
-     
-     #[account(mut)]
-     pub user: Signer<'info>,
-     pub system_program: Program<'info , System>
-
- }
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer=user, space=264)]
+    pub calculator:  Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
   #[account]
   pub struct Calculator {
      pub greeting: String,
